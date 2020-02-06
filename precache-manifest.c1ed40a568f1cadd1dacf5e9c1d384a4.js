0ed0bdf1aac6ecf9d7e3949a4af4439a91c9b8fe
pandaaan <nhudan06@gmail.com>
2020-02-06 21:42:59 +0100

    Updates

---
+++ precache-manifest.c1ed40a568f1cadd1dacf5e9c1d384a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c11e751bc4cf1ed4432db856fe61b56",
+    "url": "/webtoon-collection/index.html"
+  },
+  {
+    "revision": "c94444d066d2fd1aee85",
+    "url": "/webtoon-collection/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "79dbc70267f6ee9b88fa",
+    "url": "/webtoon-collection/static/css/main.8b779ed5.chunk.css"
+  },
+  {
+    "revision": "c94444d066d2fd1aee85",
+    "url": "/webtoon-collection/static/js/2.bef188f8.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/webtoon-collection/static/js/2.bef188f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79dbc70267f6ee9b88fa",
+    "url": "/webtoon-collection/static/js/main.4c44b07a.chunk.js"
+  },
+  {
+    "revision": "c061b2a40b44b5e42a13",
+    "url": "/webtoon-collection/static/js/runtime-main.3f2403a7.js"
+  }
+]);
